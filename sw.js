8852e4eea861eb6e5ea2ee59712c8d8d9fb75748
jonathanrondeau13-maker <jonathanrondeau13@gmail.com>
2025-12-05 11:59:50 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Nom du cache. Changer la version si vous modifiez des fichiers statiques.
+const CACHE_NAME = 'labos-calendar-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
